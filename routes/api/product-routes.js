b9884753d43e3e26fa4ec13b248b39adaa259d76
kwinahn <77373020+kwinahn@users.noreply.github.com>
2021-05-16 19:31:20 -0500

    product-routes code

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,12 +7,41 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product.findAll({
+    include: [
+      {
+        model: Category,
+        attributes: ["category_name"],
+      },
+      {
+        model: Tag,
+        attributes: ["tag_name"],
+      },
+    ],
+  })
+    .then((data) => res.json(data))
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findOne({
+    where: {
+      id: req.params.id,
+    },
+    include: [
+      {
+        model: Category,
+        attributes: ["category_name"],
+      },
+      {
+        model: Tag,
+        attributes: ["tag_name"],
+      },
+    ],
+  })
+    .then((data) => res.json(data))
 });
 
 // create new product
@@ -91,6 +120,24 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((data) => {
+      if (!data) {
+        res
+          .status(404)
+          .json({ message: "There is no product found under this ID" });
+        return;
+      }
+      res.json(data);
+    })
+    .catch((err) => {
+      //console.log(err);
+      res.status(400).json(err);
+    });
 });
 
 module.exports = router;
