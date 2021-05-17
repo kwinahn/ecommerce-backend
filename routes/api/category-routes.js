6066bb293eba2845630d05af5025d07edf064f27
kwinahn <77373020+kwinahn@users.noreply.github.com>
2021-05-16 19:24:25 -0500

    category-routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,23 +6,77 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll({
+    include: {
+      model: Product,
+      attributes: ["product_name"],
+    },
+  })
+    .then((allData) => res.json(allData))
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findOne({
+    where: {
+      id: req.params.id,
+    },
+    include: {
+      model: Product,
+      attributes: ["category_id"],
+    },
+  })
+    .then((allData) => res.json(allData))
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create({
+    category_name: req.body.category_name,
+  })
+    .then((allData) => res.json(allData))
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(
+    {
+      category_name: req.body.category_name,
+    },
+    {
+      where: {
+        id: req.params.id,
+      },
+    }
+  )
+    .then((allData) => {
+      if (!allData) {
+        res
+          .status(404)
+          .json({ message: "There are no catagories with this ID" });
+        return;
+      }
+      res.json(allData);
+    })
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((allData) => {
+      if (!allData) {
+        res
+          .json(404)
+          .json({ message: "There is no category found matching this ID" });
+        return;
+      }
+      res.json(allData);
+    })
 });
 
 module.exports = router;
